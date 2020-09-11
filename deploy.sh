13963b4de2f7fb5566c26aa0fd8a5d181a58f312
pfwang2 <664589771@qq.com>
2020-09-11 16:27:36 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:doctorFei/my-blog.git master:gh-pages
+git push -f git@github.com:doctorFei/iview-admin-pro.git master:gh-pages
 
 cd -
